a8b49dc724b3d489354b2314ae921c89841327fc
Rick Clephas <rclephas@gmail.com>
2022-06-12 12:49:00 +0200

    Update KDocs

--- libraries/stdlib/src/kotlin/annotations/NativeAnnotations.kt
+++ libraries/stdlib/src/kotlin/annotations/NativeAnnotations.kt
@@ -29,7 +29,7 @@ public expect annotation class ExperimentalObjCName()
 
 /**
  * Instructs the Kotlin compiler to use a custom Objective-C and/or Swift name for this class, property, parameter or function.
- * @param exact specifies if the name of a class, object or interface should be interpreted as the exact name.
+ * @param exact specifies if the name of a class should be interpreted as the exact name.
  * E.g. the compiler won't add a top level prefix or the outer class names to exact names.
  */
 @Target(
